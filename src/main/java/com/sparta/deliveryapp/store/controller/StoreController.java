2ade5bd6245acdd1de0d065a2dd822d0a7dddc55
oenul0 <neul.dev@gmail.com>
2025-02-15 00:10:46 +0900

    chore: build.gradle validation 의존성 추가

--- src/main/java/com/sparta/deliveryapp/store/controller/StoreController.java
+++ src/main/java/com/sparta/deliveryapp/store/controller/StoreController.java
@@ -1,10 +1,16 @@
 package com.sparta.deliveryapp.store.controller;
 
+import com.sparta.deliveryapp.store.service.StoreService;
 import lombok.RequiredArgsConstructor;
+import org.apache.catalina.Store;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class StoreController {
+    private final StoreService storeService;
 
 }
--- src/main/java/com/sparta/deliveryapp/store/service/StoreService.java
+++ src/main/java/com/sparta/deliveryapp/store/service/StoreService.java
@@ -1,8 +1,10 @@
 package com.sparta.deliveryapp.store.service;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class StoreService {
 
 }
