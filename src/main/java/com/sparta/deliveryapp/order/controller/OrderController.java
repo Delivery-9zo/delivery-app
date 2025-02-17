ad20accbd4ebdd137879d7c544118b8221e54083
minji <zz8168@naver.com>
2025-02-17 18:09:41 +0900

    fix: 로그인 정보 인증객체 추가

--- src/main/java/com/sparta/deliveryapp/order/controller/OrderController.java
+++ src/main/java/com/sparta/deliveryapp/order/controller/OrderController.java
@@ -3,8 +3,12 @@ package com.sparta.deliveryapp.order.controller;
 import com.sparta.deliveryapp.order.dto.OrderRequestDto;
 import com.sparta.deliveryapp.order.dto.OrderResponseDto;
 import com.sparta.deliveryapp.order.service.OrderService;
+import com.sparta.deliveryapp.user.security.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -15,16 +19,18 @@ import java.util.UUID;
 @RequestMapping("/api/orders")
 @RequiredArgsConstructor
 public class OrderController {
-
+    private static final Logger logger = LoggerFactory.getLogger(OrderController.class);
     private final OrderService orderService;
 
     // 주문 취소(WAIT -> CANCEL)
     // user PR 후, 권한 확인
     @PutMapping("/cancel/{orderId}")
     public ResponseEntity<?> updateOrderStateToCancel(@PathVariable("orderId")  UUID orderId,
-                                                                         @RequestBody OrderRequestDto orderRequestDto) {
+                                                      @RequestBody OrderRequestDto orderRequestDto,
+                                                      @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        logger.info("Authenticated User : {}", userDetails);
         try {
-            OrderResponseDto responseDto = orderService.updateOrderStateToCancel(orderId, orderRequestDto);
+            OrderResponseDto responseDto = orderService.updateOrderStateToCancel(orderId, orderRequestDto, userDetails.getUser());
             return ResponseEntity.ok(responseDto);
         } catch (ResponseStatusException e) {
             // ResponseStatusException 의 경우, 상태 코드와 메시지를 반환
