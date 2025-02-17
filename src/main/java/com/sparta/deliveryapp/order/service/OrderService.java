9763c1c9f3a2091fa0aa094fec3dc1e841554cbe
minji <zz8168@naver.com>
2025-02-17 18:29:00 +0900

    rename: OrderToCanceled dto 파일명 변경

--- src/main/java/com/sparta/deliveryapp/order/service/OrderService.java
+++ src/main/java/com/sparta/deliveryapp/order/service/OrderService.java
@@ -1,7 +1,7 @@
 package com.sparta.deliveryapp.order.service;
 
-import com.sparta.deliveryapp.order.dto.OrderRequestDto;
-import com.sparta.deliveryapp.order.dto.OrderResponseDto;
+import com.sparta.deliveryapp.order.dto.OrderToCanceledRequestDto;
+import com.sparta.deliveryapp.order.dto.OrderToCanceledResponseDto;
 import com.sparta.deliveryapp.order.entity.Order;
 import com.sparta.deliveryapp.order.entity.OrderState;
 import com.sparta.deliveryapp.order.repository.OrderRespository;
@@ -28,16 +28,16 @@ public class OrderService {
 
     // 주문 취소(SUCCESS -> CANCEL)
     @Transactional
-    public OrderResponseDto updateOrderStateToCancel(UUID orderId, OrderRequestDto orderRequestDto, User user) {
+    public OrderToCanceledResponseDto updateOrderStateToCancel(UUID orderId, OrderToCanceledRequestDto orderToCanceledRequestDto, User user) {
 
         logger.info("주문 상태 ToCancel 업데이트 시작 : orderId={}", orderId);
 
         try {
             Order order = orderRespository.findById(orderId)
                     .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "일치하는 주문정보가 없습니다."));
-            if (orderRequestDto.getOrderState() == null) {
+            if (orderToCanceledRequestDto.getOrderState() == null) {
                 throw new IllegalArgumentException("주문 상태가 필요합니다.");
-            } else if (!orderRequestDto.getOrderState().equals(OrderState.WAIT)) {
+            } else if (!orderToCanceledRequestDto.getOrderState().equals(OrderState.WAIT)) {
                 throw new IllegalArgumentException("주문완료 상태가 아니므로 주문취소가 불가합니다.");
             }
 
@@ -49,7 +49,7 @@ public class OrderService {
             }
 
             LocalDateTime now = LocalDateTime.now();
-            LocalDateTime orderTimeFiveMinutesLater = orderRequestDto.getOrderTime().plusMinutes(5);
+            LocalDateTime orderTimeFiveMinutesLater = orderToCanceledRequestDto.getOrderTime().plusMinutes(5);
             if (now.isAfter(orderTimeFiveMinutesLater)) {
                 throw new IllegalArgumentException("주문취소는 주문완료 후 5분 이내에만 취소 가능합니다.");
             } else {
@@ -61,7 +61,7 @@ public class OrderService {
             Order updatedOrder = orderRespository.save(order);
             logger.info("주문 상태 ToCancel 업데이트 성공: orderId={}", updatedOrder.getOrderId());
 
-            return new OrderResponseDto(updatedOrder);
+            return new OrderToCanceledResponseDto(updatedOrder);
         } catch (ResponseStatusException e) {
             logger.error("주문 상태 업데이트 오류: {}", e.getReason());
             throw e; // 예외를 다시 던져서 컨트롤러에서 처리하게 함
