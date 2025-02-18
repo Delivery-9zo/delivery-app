1053217a31ca3506b3814fa0e162eef97d1dde65
minji <zz8168@naver.com>
2025-02-18 12:00:36 +0900

    rename: dto 및 OrderStatusService 수정

--- src/main/java/com/sparta/deliveryapp/order/controller/OrderController.java
+++ src/main/java/com/sparta/deliveryapp/order/controller/OrderController.java
@@ -1,8 +1,8 @@
 package com.sparta.deliveryapp.order.controller;
 
-import com.sparta.deliveryapp.order.dto.OrderToCanceledRequestDto;
-import com.sparta.deliveryapp.order.dto.OrderToCanceledResponseDto;
-import com.sparta.deliveryapp.order.service.OrderService;
+import com.sparta.deliveryapp.order.dto.CancellationOrderRequestDto;
+import com.sparta.deliveryapp.order.dto.CancellationOrderResponseDto;
+import com.sparta.deliveryapp.order.service.OrderStatusService;
 import com.sparta.deliveryapp.user.security.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
@@ -20,16 +20,16 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class OrderController {
     private static final Logger logger = LoggerFactory.getLogger(OrderController.class);
-    private final OrderService orderService;
+    private final OrderStatusService orderStatusService;
 
     // 주문 취소(SUCCESS -> CANCEL)
     @PutMapping("/cancel/{orderId}")
     public ResponseEntity<?> updateOrderStateToCancel(@PathVariable("orderId")  UUID orderId,
-                                                      @RequestBody OrderToCanceledRequestDto orderToCanceledRequestDto,
+                                                      @RequestBody CancellationOrderRequestDto cancellationOrderRequestDto,
                                                       @AuthenticationPrincipal UserDetailsImpl userDetails) {
         logger.info("Authenticated User : {}", userDetails);
         try {
-            OrderToCanceledResponseDto responseDto = orderService.updateOrderStateToCancel(orderId, orderToCanceledRequestDto, userDetails.getUser());
+            CancellationOrderResponseDto responseDto = orderStatusService.updateOrderStateToCancel(orderId, cancellationOrderRequestDto, userDetails.getUser());
             return ResponseEntity.ok(responseDto);
         } catch (ResponseStatusException e) {
             // ResponseStatusException 의 경우, 상태 코드와 메시지를 반환
--- src/main/java/com/sparta/deliveryapp/order/service/OrderStatusService.java
+++ src/main/java/com/sparta/deliveryapp/order/service/OrderStatusService.java
@@ -1,7 +1,7 @@
 package com.sparta.deliveryapp.order.service;
 
-import com.sparta.deliveryapp.order.dto.OrderToCanceledRequestDto;
-import com.sparta.deliveryapp.order.dto.OrderToCanceledResponseDto;
+import com.sparta.deliveryapp.order.dto.CancellationOrderRequestDto;
+import com.sparta.deliveryapp.order.dto.CancellationOrderResponseDto;
 import com.sparta.deliveryapp.order.entity.Order;
 import com.sparta.deliveryapp.order.entity.OrderState;
 import com.sparta.deliveryapp.order.repository.OrderRespository;
@@ -21,23 +21,23 @@ import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
-public class OrderService {
-    private static final Logger logger = LoggerFactory.getLogger(OrderService.class);
+public class OrderStatusService {
+    private static final Logger logger = LoggerFactory.getLogger(OrderStatusService.class);
     private final OrderRespository orderRespository;
     private final UserRepository userRepository;
 
     // 주문 취소(SUCCESS -> CANCEL)
     @Transactional
-    public OrderToCanceledResponseDto updateOrderStateToCancel(UUID orderId, OrderToCanceledRequestDto orderToCanceledRequestDto, User user) {
+    public CancellationOrderResponseDto updateOrderStateToCancel(UUID orderId, CancellationOrderRequestDto cancellationOrderRequestDto, User user) {
 
         logger.info("주문 상태 ToCancel 업데이트 시작 : orderId={}", orderId);
 
         try {
             Order order = orderRespository.findById(orderId)
                     .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "일치하는 주문정보가 없습니다."));
-            if (orderToCanceledRequestDto.getOrderState() == null) {
+            if (cancellationOrderRequestDto.getOrderState() == null) {
                 throw new IllegalArgumentException("주문 상태가 필요합니다.");
-            } else if (!orderToCanceledRequestDto.getOrderState().equals(OrderState.WAIT)) {
+            } else if (!cancellationOrderRequestDto.getOrderState().equals(OrderState.WAIT)) {
                 throw new IllegalArgumentException("주문완료 상태가 아니므로 주문취소가 불가합니다.");
             }
 
@@ -49,7 +49,7 @@ public class OrderService {
             }
 
             LocalDateTime now = LocalDateTime.now();
-            LocalDateTime orderTimeFiveMinutesLater = orderToCanceledRequestDto.getOrderTime().plusMinutes(5);
+            LocalDateTime orderTimeFiveMinutesLater = cancellationOrderRequestDto.getOrderTime().plusMinutes(5);
             if (now.isAfter(orderTimeFiveMinutesLater)) {
                 throw new IllegalArgumentException("주문취소는 주문완료 후 5분 이내에만 취소 가능합니다.");
             } else {
@@ -61,7 +61,7 @@ public class OrderService {
             Order updatedOrder = orderRespository.save(order);
             logger.info("주문 상태 ToCancel 업데이트 성공: orderId={}", updatedOrder.getOrderId());
 
-            return new OrderToCanceledResponseDto(updatedOrder);
+            return new CancellationOrderResponseDto(updatedOrder);
         } catch (ResponseStatusException e) {
             logger.error("주문 상태 업데이트 오류: {}", e.getReason());
             throw e; // 예외를 다시 던져서 컨트롤러에서 처리하게 함
