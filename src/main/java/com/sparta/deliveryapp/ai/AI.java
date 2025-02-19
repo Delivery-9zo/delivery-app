c2ac545b1289225ac337123bf6518b7289c724dc
Jong-Min <codejomo99@gmail.com>
2025-02-19 14:54:28 +0900

    feat: User , AI 연관매핑 추가

--- src/main/java/com/sparta/deliveryapp/ai/AI.java
+++ src/main/java/com/sparta/deliveryapp/ai/AI.java
@@ -12,6 +12,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.ManyToAny;
 import org.hibernate.annotations.UuidGenerator;
 
 @Entity
