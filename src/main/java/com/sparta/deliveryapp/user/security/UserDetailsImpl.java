9dfe8cfa6475b9ae2726872ef30f92cdd82c3010
Jong-Min <codejomo99@gmail.com>
2025-02-20 12:32:30 +0900

    refactor: 불 필요한 import

--- src/main/java/com/sparta/deliveryapp/user/security/UserDetailsImpl.java
+++ src/main/java/com/sparta/deliveryapp/user/security/UserDetailsImpl.java
@@ -6,7 +6,6 @@ import java.util.ArrayList;
 import java.util.Collection;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-
 import org.springframework.security.core.userdetails.UserDetails;
 
 public class UserDetailsImpl implements UserDetails {
