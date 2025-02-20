e3e3d657d8735aec1e4c760e331edbc0e44aba84
sodychoe <sodychoe@icloud.com>
2025-02-20 09:50:42 +0900

    fix: StoreRepository 의 ID 타입 수정

--- src/main/java/com/sparta/deliveryapp/store/repository/StoreRepository.java
+++ src/main/java/com/sparta/deliveryapp/store/repository/StoreRepository.java
@@ -10,7 +10,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface StoreRepository extends JpaRepository<Store, Integer> {
+public interface StoreRepository extends JpaRepository<Store, UUID> {
 
   Optional<Store> findByStoreName(@NotBlank String storeName);
 
