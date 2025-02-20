9b419983567399f4538067966eba36869a99b328
oneul0 <neul.dev@gmail.com>
2025-02-20 11:39:19 +0900

    SCRUM-25 feat: 경위도로 가게 찾기 기능 추가
    - postgis 설정을 위한 docker-compose.yaml 설정 추가
    - storecontroller에 경위도로 가게 찾기 api 추가
    - Store 엔티티에 경위도 검색을 위한 Point 임시 컬럼 추가
    - build.gradle에 netty, queryDSL, hibernate-spatial 의존성 추가

--- src/main/java/com/sparta/deliveryapp/store/repository/StoreRepository.java
+++ src/main/java/com/sparta/deliveryapp/store/repository/StoreRepository.java
@@ -7,6 +7,8 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -19,4 +21,19 @@ public interface StoreRepository extends JpaRepository<Store, UUID> {
   Optional<Store> findByStoreId(UUID uuid);
 
   List<Store> findByStoreNameContaining(String storeName);
+
+  @Query(value = """
+          SELECT *
+          FROM p_store s
+          WHERE ST_DWithin(
+              geography(ST_SetSRID(ST_Point(s.store_x, s.store_y), 4326)),
+              geography(ST_SetSRID(ST_Point(:longitude, :latitude), 4326)),
+              :range
+          )
+      """, nativeQuery = true)
+  List<Object[]> findNearbyStoresWithoutCategory(@Param("longitude") double longitude,
+      @Param("latitude") double latitude,
+      @Param("range") int range);
+
+
 }
