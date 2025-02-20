8c96cdd65be2df5762f1f9c4b6e2c9ee7e8dfecf
Jong-Min <codejomo99@gmail.com>
2025-02-20 11:28:57 +0900

    test: 유저 로그인시 테스트 코드

--- src/test/java/com/sparta/deliveryapp/user/UserServiceTest.java
+++ src/test/java/com/sparta/deliveryapp/user/UserServiceTest.java
@@ -1,10 +1,12 @@
 package com.sparta.deliveryapp.user;
 
-import static org.mockito.Mockito.*;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import com.sparta.deliveryapp.user.dto.SignInRequestDto;
-import com.sparta.deliveryapp.user.dto.SignInResponseDto;
 import com.sparta.deliveryapp.user.dto.SignUpRequestDto;
 import com.sparta.deliveryapp.user.entity.User;
 import com.sparta.deliveryapp.user.entity.UserRole;
@@ -12,10 +14,12 @@ import com.sparta.deliveryapp.user.jwt.JwtUtil;
 import com.sparta.deliveryapp.user.repository.UserRepository;
 import com.sparta.deliveryapp.user.service.UserService;
 import java.util.Optional;
+import java.util.UUID;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.*;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 class UserServiceTest {
@@ -77,6 +81,57 @@ class UserServiceTest {
     userService.signUp(requestDto);
 
     verify(userRepository).save(any(User.class)); // 사용자 저장 호출 확인
+  }
+
+
+  @Test
+  @DisplayName("회원이 존재하지 않는 경우 예외처리")
+  void signIn_whenUserNotFound_throwsException(){
+    SignInRequestDto requestDto = new SignInRequestDto("test@example.com", "password123");
+
+    when(userRepository.findByEmail(requestDto.getEmail())).thenReturn(Optional.empty());
+
+    IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+      userService.signIn(requestDto);
+    });
+
+    assertEquals("회원이 존재하지 않습니다.", exception.getMessage());
+
+  }
+
+  @Test
+  @DisplayName("로그인시 비밀번호가 다를때 예외처리")
+  void signIn_whenInvalidPassword_throwsException() {
+    SignInRequestDto requestDto = new SignInRequestDto("test@example.com", "wrongPassword");
+    User user = new User( UUID.randomUUID(),"test", "nickname", "1234","testaddress","test@test.com",UserRole.CUSTOMER);
+
+    when(userRepository.findByEmail(requestDto.getEmail())).thenReturn(Optional.of(user));
+    when(passwordEncoder.matches(requestDto.getPassword(), user.getPassword())).thenReturn(false);
+
+    IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+      userService.signIn(requestDto);
+    });
+
+    assertEquals("비밀번호가 다릅니다.", exception.getMessage());
+  }
+
+  @Test
+  @DisplayName("로그인시 토큰 생성")
+  void signIn_whenCredentialsAreValid_returnsToken() {
+
+    // given
+    SignInRequestDto requestDto = new SignInRequestDto("test@example.com", "password123");
+    User user = new User( UUID.randomUUID(),"test", "nickname", "1234","testaddress","test@test.com",UserRole.CUSTOMER);
+
+    // when
+    when(userRepository.findByEmail(requestDto.getEmail())).thenReturn(Optional.of(user));
+    when(passwordEncoder.matches(requestDto.getPassword(), user.getPassword())).thenReturn(true);
+    when(jwtUtil.createToken(user.getEmail(), user.getRole())).thenReturn("jwt-token");
+
+    // 서비스 로직에서 로그인시 토큰을 생성해 주기 때문에 검사
+    String token = userService.signIn(requestDto);
+
+    assertEquals("jwt-token", token);
   }
 
 }
