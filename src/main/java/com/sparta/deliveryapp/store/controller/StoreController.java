eef47e133698c4570b0dd00bd4c2955c08b8312b
oneul0 <neul.dev@gmail.com>
2025-02-20 11:39:19 +0900

    SCRUM-25 feat: 경위도로 가게 찾기 기능 추가
    - postgis 설정을 위한 docker-compose.yaml 설정 추가
    - storecontroller에 경위도로 가게 찾기 api 추가
    - Store 엔티티에 경위도 검색을 위한 Point 임시 컬럼 추가
    - build.gradle에 netty, queryDSL, hibernate-spatial 의존성 추가

--- src/main/java/com/sparta/deliveryapp/store/controller/StoreController.java
+++ src/main/java/com/sparta/deliveryapp/store/controller/StoreController.java
@@ -1,18 +1,25 @@
 package com.sparta.deliveryapp.store.controller;
 
+import com.sparta.deliveryapp.store.dto.StoreNearbyStoreResponseDto;
 import com.sparta.deliveryapp.store.dto.StoreRequestDto;
+import com.sparta.deliveryapp.store.dto.StoreResponseDto;
 import com.sparta.deliveryapp.store.service.StoreService;
 import com.sparta.deliveryapp.user.security.UserDetailsImpl;
 import jakarta.validation.Valid;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/stores")
@@ -29,6 +36,21 @@ public class StoreController {
     storeService.regiStore(storeRequestDto, userDetails);
 
     return ResponseEntity.ok().body("가게가 정상적으로 등록되었습니다.");
+  }
+
+  //카테고리 없이 경위도로만 근처 가게 검색
+  @GetMapping("location/{longitude}/{latitude}")
+  @PreAuthorize("isAuthenticated()")
+  public ResponseEntity<List<StoreNearbyStoreResponseDto>> getNearbyStoresWithoutCategory(
+      @PathVariable double longitude,
+      @PathVariable double latitude,
+      @AuthenticationPrincipal UserDetailsImpl userDetails) {
+
+    List<StoreNearbyStoreResponseDto> storeResponseDto = storeService.findNearbyStoresWithoutCategory(longitude, latitude, userDetails);
+
+    log.info(storeResponseDto.toString());
+    //todo: 커스텀 AccessDenied 예외 처리 추가(GlobalExceptionHandler에)
+    return ResponseEntity.ok().body(storeResponseDto);
   }
 
 
