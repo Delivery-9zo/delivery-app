61528b229fb1891e87e2524b28ee303076fa0fe9
sodychoe <sodychoe@icloud.com>
2025-02-21 19:38:51 +0900

    feat: SCRUM-67 리뷰 컨트롤러 생성 메서드 구현

--- src/main/java/com/sparta/deliveryapp/review/controller/ReviewController.java
+++ src/main/java/com/sparta/deliveryapp/review/controller/ReviewController.java
@@ -1,8 +1,38 @@
 package com.sparta.deliveryapp.review.controller;
 
+import com.sparta.deliveryapp.review.dto.ReviewPostRequestDto;
+import com.sparta.deliveryapp.review.service.ReviewService;
+import com.sparta.deliveryapp.user.security.UserDetailsImpl;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/reviews")
+@RequiredArgsConstructor
 public class ReviewController {
+
+  private final ReviewService reviewService;
+
+  @PostMapping("/{storeId}")
+  public ResponseEntity<String> postReview(
+      @RequestBody ReviewPostRequestDto dto,
+      @PathVariable(name = "storeId") UUID storeId,
+      @AuthenticationPrincipal UserDetailsImpl userDetails
+  ) {
+    reviewService.postReview(dto, storeId, userDetails);
+    return ResponseEntity
+        .status(HttpStatus.OK)
+        .contentType(MediaType.APPLICATION_JSON)
+        .body("리뷰가 등록되었습니다.");
+  }
 
 }
