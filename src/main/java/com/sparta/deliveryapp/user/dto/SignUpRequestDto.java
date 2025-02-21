e60d6611be486d7b4c13d38fe1eecd2fbe2fdeff
Jong-Min <codejomo99@gmail.com>
2025-02-21 12:35:43 +0900

    feat: NoArgs,AllArgs 추가

--- src/main/java/com/sparta/deliveryapp/user/dto/SignUpRequestDto.java
+++ src/main/java/com/sparta/deliveryapp/user/dto/SignUpRequestDto.java
@@ -5,13 +5,17 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class SignUpRequestDto {
 
   @NotEmpty(message = "User name cannot be empty")
