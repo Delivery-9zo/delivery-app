27be22d3dd997ff48806f2a25620c39e57c9f567
Jong-Min <codejomo99@gmail.com>
2025-02-21 12:51:15 +0900

    refactor

--- src/main/java/com/sparta/deliveryapp/user/dto/SignInRequestDto.java
+++ src/main/java/com/sparta/deliveryapp/user/dto/SignInRequestDto.java
@@ -1,8 +1,6 @@
 package com.sparta.deliveryapp.user.dto;
 
 
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
@@ -14,13 +12,9 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class SignInRequestDto {
 
-  @Parameter(description = "사용자의 이메일")
-  @Schema(description = "사용자의 이메일을 입력하세요")
   @NotNull(message = "User Email cannot be null")
   private String email;
 
-  @Parameter(description = "사용자의 비밀번호")
-  @Schema(description = "비밀번호을 입력하세요")
   @NotEmpty(message = "Password cannot be empty")
   private String password;
 
