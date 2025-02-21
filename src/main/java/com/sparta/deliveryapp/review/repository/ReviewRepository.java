bcda96206eb7de04afb6a665d664eb69e33a020a
sodychoe <sodychoe@icloud.com>
2025-02-21 23:43:19 +0900

    feat: SCRUM-68 리뷰 조회 리포지토리 메서드 구현

--- src/main/java/com/sparta/deliveryapp/review/repository/ReviewRepository.java
+++ src/main/java/com/sparta/deliveryapp/review/repository/ReviewRepository.java
@@ -2,8 +2,13 @@ package com.sparta.deliveryapp.review.repository;
 
 import com.sparta.deliveryapp.review.entity.Review;
 import java.util.UUID;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReviewRepository extends JpaRepository<Review, UUID> {
 
+  Page<Review> findAllByStore_StoreId(UUID storeId, Pageable pageable);
+
+  Page<Review> findAllByUser_Email(String email, Pageable pageable);
 }
