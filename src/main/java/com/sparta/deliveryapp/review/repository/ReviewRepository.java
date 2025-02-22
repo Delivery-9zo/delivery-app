dec6957ea54ab4e6436f5110d623ceb50ea11b88
sodychoe <sodychoe@icloud.com>
2025-02-22 15:55:22 +0900

    feat: SCRUM-87 리뷰 평점 기능 리포지토리 구현

--- src/main/java/com/sparta/deliveryapp/review/repository/ReviewRepository.java
+++ src/main/java/com/sparta/deliveryapp/review/repository/ReviewRepository.java
@@ -1,12 +1,15 @@
 package com.sparta.deliveryapp.review.repository;
 
 import com.sparta.deliveryapp.review.entity.Review;
+import com.sparta.deliveryapp.review.repository.querydsl.ReviewRepositoryCustom;
 import java.util.UUID;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ReviewRepository extends JpaRepository<Review, UUID> {
+public interface ReviewRepository extends
+    JpaRepository<Review, UUID>,
+    ReviewRepositoryCustom {
 
   Page<Review> findAllByStore_StoreId(UUID storeId, Pageable pageable);
 
