33f120e7b28dbf3448de109ed462e6a7df1e2833
minji <zz8168@naver.com>
2025-02-22 23:15:57 +0900

    SCRUM-28 fix: 주문상세 엔터티 다대일 및 baseEntity 설정

--- src/main/java/com/sparta/deliveryapp/order/entity/OrderItem.java
+++ src/main/java/com/sparta/deliveryapp/order/entity/OrderItem.java
@@ -1,5 +1,6 @@
 package com.sparta.deliveryapp.order.entity;
 
+import com.sparta.deliveryapp.auditing.BaseEntity;
 import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.UuidGenerator;
@@ -12,7 +13,7 @@ import java.util.UUID;
 @AllArgsConstructor
 @NoArgsConstructor
 @Table(name = "p_orderItem")
-public class OrderItem {
+public class OrderItem extends BaseEntity {
 
     @Id
     @GeneratedValue
@@ -29,10 +30,15 @@ public class OrderItem {
     @Column(name = "quantity")
     private int quantity;
 
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
     @Builder
-    public OrderItem(UUID userId, UUID menuId, int quantity) {
+    public OrderItem(UUID userId, UUID menuId, int quantity, Order order) {
         this.userId = userId;
         this.menuId = menuId;
         this.quantity = quantity;
+        this.order = order;
     }
 }
