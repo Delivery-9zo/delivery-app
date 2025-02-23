b87d93dec023f822c4f933bb9bb7d9ffd82fc5fc
minji <zz8168@naver.com>
2025-02-24 03:46:31 +0900

    feat : swagger 추가 - Request Body에 @Schema, @Parameter 적용

--- src/main/java/com/sparta/deliveryapp/order/dto/RegisterOrderRequestDto.java
+++ src/main/java/com/sparta/deliveryapp/order/dto/RegisterOrderRequestDto.java
@@ -3,6 +3,8 @@ package com.sparta.deliveryapp.order.dto;
 import com.sparta.deliveryapp.order.entity.Order;
 import com.sparta.deliveryapp.order.entity.OrderState;
 import com.sparta.deliveryapp.order.entity.OrderType;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,12 +18,24 @@ import java.util.UUID;
 public class RegisterOrderRequestDto {
 
 //    private UUID menuId;
+    @Parameter(description = "메뉴 수량")
+    @Schema(description = "메뉴 수량을 입력하세요.")
     private int quantity;
+    @Parameter(description = "주문 타입")
+    @Schema(description = "주문 타입(NON_FACE_TO_FACE, FACE_TO_FACE)을 입력하세요.")
     private OrderType orderType;
+    @Parameter(description = "메뉴 가격")
+    @Schema(description = "메뉴 가격이 필요합니다.")
     private int price;
 
+    @Parameter(description = "사용자 주소")
+    @Schema(description = "사용자의 배달 주소를 입력하세요.")
     private String userAddress;
+    @Parameter(description = "주문 메모")
+    @Schema(description = "주문 메모를 작성하세요.")
     private String orderMemo;
+    @Parameter(description = "주문 상태")
+    @Schema(description = "주문 상태가 필요합니다.")
     private OrderState orderState;
     private List<RegisterOrderItemRequestDto> registerOrderItemRequestDtoList;
 
--- src/main/java/com/sparta/deliveryapp/payment/dto/RegisterPaymentRequestDto.java
+++ src/main/java/com/sparta/deliveryapp/payment/dto/RegisterPaymentRequestDto.java
@@ -1,5 +1,7 @@
 package com.sparta.deliveryapp.payment.dto;
 
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
@@ -13,9 +15,13 @@ import java.util.UUID;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class RegisterPaymentRequestDto {
 
+    @Parameter(description = "주문 uuid")
+    @Schema(description = "사용자의 주문 uuid를 입력하세요.")
     @NotNull(message = "Order Id cannot be null")
     private UUID orderId;
 
+    @Parameter(description = "결제 금액")
+    @Schema(description = "결제 금액이 필요합니다.")
     @NotNull(message = "결제 금액은 필수입니다.")
     @Min(value = 1, message = "결제 금액은 1원 이상 입력되어야 합니다.")
     private int paymentAmount;
