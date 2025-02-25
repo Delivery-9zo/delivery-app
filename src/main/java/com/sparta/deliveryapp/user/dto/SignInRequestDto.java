fa67ed78c0670d5ea92f7a1d660f1b9e1aec6827
Jong-Min <codejomo99@gmail.com>
2025-02-25 14:05:56 +0900

    docs: swagger 문서화 작성

--- src/main/java/com/sparta/deliveryapp/user/dto/SignInRequestDto.java
+++ src/main/java/com/sparta/deliveryapp/user/dto/SignInRequestDto.java
@@ -1,6 +1,7 @@
 package com.sparta.deliveryapp.user.dto;
 
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
@@ -13,9 +14,11 @@ import lombok.NoArgsConstructor;
 public class SignInRequestDto {
 
   @NotNull(message = "User Email cannot be null")
+  @Schema(description = "유저 이메일",example = "master@test.com")
   private String email;
 
   @NotEmpty(message = "Password cannot be empty")
+  @Schema(description = "유저 비밀번호",example = "1234")
   private String password;
 
 }
--- src/main/java/com/sparta/deliveryapp/user/dto/UserUpdateRequestDto.java
+++ src/main/java/com/sparta/deliveryapp/user/dto/UserUpdateRequestDto.java
@@ -1,6 +1,7 @@
 package com.sparta.deliveryapp.user.dto;
 
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,9 +10,17 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserUpdateRequestDto {
+
+  @Schema(description = "유저 이름",example = "수정한 이름")
   private String userName;
+
+  @Schema(description = "유저 닉네임",example = "수정한 이메일")
   private String nickName;
+
+  @Schema(description = "유저 비밀번호",example = "1234")
   private String password;
+
+  @Schema(description = "유저 주소",example = "경기도 수풍로 90")
   private String userAddress;
 
 }
