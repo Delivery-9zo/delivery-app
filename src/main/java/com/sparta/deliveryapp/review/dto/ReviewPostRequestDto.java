7ea222adb56dae9cc9b967e2ba835d92ab89cb08
sodychoe <sodychoe@icloud.com>
2025-02-26 10:46:37 +0900

    docs: 리뷰 도메인 스웨거 설명 추가

--- src/main/java/com/sparta/deliveryapp/review/dto/ReviewPostRequestDto.java
+++ src/main/java/com/sparta/deliveryapp/review/dto/ReviewPostRequestDto.java
@@ -1,8 +1,16 @@
 package com.sparta.deliveryapp.review.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public record ReviewPostRequestDto(
+
+    @Schema(description = "리뷰 내용", example = "맛있어요")
     String comment,
+
+    @Schema(description = "리뷰 사진 URL", example = "test.com/image/")
     String image,
+
+    @Schema(description = "리뷰 점수", example = "3")
     Integer rating
 ) {
 
