5af1a9addebf311b84ad5a980e39c971699535cc
Jong-Min <codejomo99@gmail.com>
2025-03-04 22:17:36 +0900

    feat: 리프레쉬 엔드포인트 기능 추가 #57

--- src/main/java/com/sparta/deliveryapp/user/controller/UserController.java
+++ src/main/java/com/sparta/deliveryapp/user/controller/UserController.java
@@ -3,6 +3,7 @@ package com.sparta.deliveryapp.user.controller;
 
 import com.sparta.deliveryapp.user.dto.*;
 import com.sparta.deliveryapp.user.security.UserDetailsImpl;
+import com.sparta.deliveryapp.user.service.RefreshTokenService;
 import com.sparta.deliveryapp.user.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -28,6 +29,7 @@ import java.util.Map;
 public class UserController {
 
   private final UserService userService;
+  private final RefreshTokenService refreshTokenService;
 
   @Operation(summary = "회원가입 기능", description = "회원가입 하는 api")
   @ApiResponse(responseCode = "200",description = "회원가입 성공")
@@ -86,6 +88,13 @@ public class UserController {
     String email = userDetails.getUser().getEmail();
     return userService.getUser(email, userDetails.getUser());
 
+  }
+
+  @PostMapping("/refresh-token")
+  public ResponseEntity<String> postRefreshToken(@RequestBody RefreshTokenRequestDto requestDto){
+    String newToken = refreshTokenService.refreshAccessToken(requestDto.getEmail(), requestDto.getToken());
+
+    return ResponseEntity.ok().header("Authorization", newToken).body("token: "+newToken);
   }
 
 
---
+++ src/main/java/com/sparta/deliveryapp/user/dto/RefreshTokenRequestDto.java
@@ -0,0 +1,15 @@
+package com.sparta.deliveryapp.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class RefreshTokenRequestDto {
+  private String email;
+  private String token;
+}
